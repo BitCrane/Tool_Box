5b4edc7f507984f01be7306dc7a04dc75f85d000
Mark.Z <magadeath@gmail.com>
2015-04-09 09:27:57 +0800

    Update Serial.sh

--- Serial.sh
+++ Serial.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #This is a script write serial number to the board automatically.
-#The serial number start from BPCB-1504-0000 to BPCB-1504-9999, you should only input the last 4 digit.
+#The serial number this batch is start from BPCB-1504-0000 to BPCB-1504-9999, you should only input the last 4 digit.
 #The Script will automatically check if the Yoliboard is plugged.
 #Will write all the serial number have writted into file serialwritten.txt within the same directory.
 #It need "hcm" to placed in the user binary PATH(/usr/local/bin).
 
-read -p "please enter serial number >"
-USB=$( lsusb | grep 297c )
-if [[ $REPLY =~ ^[0-9]{4}$ ]]; then
+read -p "please enter the last 4 digits of a serial number >"
+USB=$( lsusb | grep 297c ) #get the USB status
+if [[ $REPLY =~ ^[0-9]{4}$ ]]; then 
 	echo "The serial number will write is BPCB-1504-$REPLY "
-	if [[ $USB =~ 0001 ]]; then
+	if [[ $USB =~ 0001 ]]; then #determin if the Yoliboard is plugged.
 	hcm --putname "BPCB-1504-$REPLY"
 	sleep 2s
 	hcm --getname >> ./serialwritten.txt
 	cat ./serialwritten.txt
 	else
-	echo "The USB device is not plugged." >&2
+	echo "The Yoliboard is not plugged,please check the USB cable or press the reset button." >&2
 	exit 1
 	fi
 else
-	echo "The serial number input is invalid" >&2
+	echo "The input serial number is invalid" >&2
 	exit 1
 fi
 
